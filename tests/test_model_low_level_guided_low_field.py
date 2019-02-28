506be39b5f3ee3e1b5df571a9e37bcc6d445d827
nemo <ly_nudt@126.com>
2019-02-28 22:13:23 +0800

    test

--- tests/test_model_low_level_guided_low_field.py
+++ tests/test_model_low_level_guided_low_field.py
@@ -8,17 +8,23 @@ from kitty.core import KittyException
 from kitty.model.low_level.guided_low_field import DetField
 from kitty.model.low_level.encoder import strToBytes
 
+
 class DetFieldTests(BaseTestCase):
 
     def setUp(self):
-        super(DetField, self).setup(DetField)
+        super(DetFieldTests, self).setUp(DetField)
 
-    def get_field(self, value=b'\x12\x34\x12\x34\x12\x34\x12\x34\x12\x34', arith_max=32):
+    def get_field(self, value=b'\x00\x00\x00\x00', arith_max=32):
         return DetField(value=value, arith_max=arith_max)
 
-    def _testBase(self, value, num_bits_to_flip, expected_mutations):
+    def _testBase(self, value,  expected_mutations):
         len_in_bits = len(value) * 8
-        uut = self.get_field(value=value, num_bits=num_bits_to_flip)
-        self.assertEqual(uut.num_mutations(), len_in_bits - num_bits_to_flip + 1)
+        arith_max = 32
+        uut = self.get_field(value=value)
+        self.assertEqual(uut.num_mutations(),  len_in_bits * 3 - 4 + len(value)* 3 - 4 + (10 * len(value) - 16) * arith_max)
         mutations = map(lambda x: x.tobytes(), self.get_all_mutations(uut))
         self.assertEqual(set(mutations), set(expected_mutations))
+
+    def testFlipSingleBitOnSingleByte(self):
+        expected_mutations = map(lambda i: strToBytes(chr(1 << i)), range(8))
+        self._testBase(b'\x00\x00\x00\x00', expected_mutations)
