a39ce86835f0edfe0de55372c077fe3d50b35a74
ly <ly_nudt@126.com>
2019-04-16 16:12:18 +0800

    write calibrate_case

--- kitty/fuzzers/guidedfuzzer.py
+++ kitty/fuzzers/guidedfuzzer.py
@@ -2,8 +2,12 @@
 """
 
 """
+import time
 import traceback
 from kitty.fuzzers.base import BaseFuzzer
+from kitty.data.report import Report
+
+CAL_CYCLES = 8
 
 
 class GuidedFuzzer(BaseFuzzer):
@@ -14,13 +18,14 @@ class GuidedFuzzer(BaseFuzzer):
     the path, and mutating the last request.
     '''
 
-    def __init__(self, name='GuidedFuzzer', logger=None, option_line=None):
+    def __init__(self, name='GuidedFuzzer', logger=None, option_line=None, fast_cal=False):
         '''
         :param name: name of the object
         :param logger: logger for the object (default: None)
         :param option_line: cmd line options to the fuzzer
         '''
         super(GuidedFuzzer, self).__init__(name, logger, option_line)
+        self.fast_cal = fast_cal
 
     def _start(self):
         self.logger.info('should keep running? %s' % self._keep_running())
@@ -35,20 +40,19 @@ class GuidedFuzzer(BaseFuzzer):
         self._end_message()
 
     def _test_environment(self):
-        sequence = self.model.get_sequence()
         try:
-            if self._perform_dry_run(sequence):
+            if self._perform_dry_run():
                 raise Exception('Environment test failed')
         except:
             self.logger.info('Environment test failed')
             raise
 
-    def _perform_dry_run(self, sequence):
+    def _perform_dry_run(self):
         queue_entry = self.model.get_queue()
         cal_failures = 0
         q = queue_entry._queue
         while q:
-
+            res = self.calibrate_case(q)
 
 
 
@@ -56,6 +60,23 @@ class GuidedFuzzer(BaseFuzzer):
 
     def _pre_test(self):
         pass
+
+    def calibrate_case(self, queue):
+        """
+        Calibrate a new test case. This is done when processing the input directory
+        to warn about flaky or otherwise problematic test cases early on; and when
+        new paths are discovered to detect variable behavior and so on.
+        :return:
+        """
+        stage_max = 3 if self.fast_cal else CAL_CYCLES
+        self._test_info()
+        session_data = self.target.get_session_data()
+        sequence = queue.sequence
+        start_us = int(time.time() * 1000)
+        for i in range(0, stage_max):
+            cksum = 0
+            res = self._run_sequence(sequence)
+
 
     def _run_sequence(self, sequence):
         '''
@@ -90,3 +111,28 @@ class GuidedFuzzer(BaseFuzzer):
         except Exception as e:
             self.logger.error('Error in transmit: %s', e)
             raise
+
+    def _post_test(self):
+        '''
+        :return: True if test failed
+        '''
+        failure_detected = False
+        self.target.post_test(self.model.current_index())
+        report = self._get_report()
+        status = report.get_status()
+        if self._in_environment_test:
+            return status != Report.PASSED
+        if status != Report.PASSED:
+            self._store_report(report)
+            self.user_interface.failure_detected()
+            failure_detected = True
+            self.logger.warning('!! Failure detected !!')
+        elif self.config.store_all_reports:
+            self._store_report(report)
+        if failure_detected:
+            self.session_info.failure_count += 1
+        self._store_session()
+        if self.config.delay_secs:
+            self.logger.debug('delaying for %f seconds', self.config.delay_secs)
+            time.sleep(self.config.delay_secs)
+        return failure_detected
