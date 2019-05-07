ada06eb1ea4c42b8dcaacd705b9badfc0b252d99
ly <ly_nudt@126.com>
2019-05-08 07:37:14 +0800

    fix sancov file

--- examples/01_file_generator/runner5.py
+++ examples/01_file_generator/runner5.py
@@ -5,7 +5,7 @@ from kitty.model import String
 from kitty.model import Template
 from kitty.model import GuidedModel
 t1 = Template(name='T1', fields=[
-    String('The default string', name='S1_1'),
+    String('The def\nault string\n', name='S1_1'),
     String('Another string', name='S1_2'),
 ])
 t2 = Template(name='T2', fields=[
@@ -27,7 +27,7 @@ model.connect(t2, t4)
 model.connect(t1, t3)
 model.connect(t3, t4)
 seq = model.get_sequence()
-at = ApplicationTarget(name="at",path="/home/ly/abc/2-4cov.out",args=[],targetfile="/home/ly/abc/2-4cov.out.18951.sancov")
+at = ApplicationTarget(name="at", path="/home/ly/abc/2-4cov.out", args=[], sancov_path="/home/ly/abc/sancov", env={"ASAN_OPTIONS":"coverage=1:coverage_direct=1:coverage_dir='/home/ly/abc/sancov':log_path='/home/ly/abc/sancov_log'"})
 fuzzer = GuidedFuzzer(name="Example 1 - File Generator")
 fuzzer.set_interface(WebInterface(port=26004))
 fuzzer.set_model(model)
