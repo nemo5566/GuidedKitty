803a6398cd1a253f9f51becd8b5e7548bac47d45
ly <ly_nudt@126.com>
2019-05-07 15:29:06 +0800

    fix sancov file

--- kitty/targets/guidedtarget.py
+++ kitty/targets/guidedtarget.py
@@ -5,6 +5,9 @@
 
 import struct
 import sys
+import os
+import array
+import bisect
 import time
 import traceback
 from binascii import hexlify
@@ -23,7 +26,7 @@ class GuidedTarget(BaseTarget):
     Target that does nothing. Weird, but sometimes it is required.
     '''
 
-    def __init__(self, name, logger=None, expect_response=False, tfile=None):
+    def __init__(self, name, logger=None, expect_response=False, sancov_path=None):
         '''
         :param name: name of the target
         :param logger: logger for this object (default: None)
@@ -35,7 +38,7 @@ class GuidedTarget(BaseTarget):
         self.receive_failure = False
         self.transmission_count = 0
         self.transmission_report = None
-        self.tfile = tfile
+        self.sancov_path = sancov_path
 
 
     def _check_bits(self, bits):
@@ -81,6 +84,80 @@ class GuidedTarget(BaseTarget):
         f.close()
         # self.logger.debug("sancov: read %d %d-bit PCs from %s" % (size * 8 / bits, bits, path))
         return s
+
+    def sancov_analyse(self, raw, map):
+        mem_map = list()
+        with open(map, mode="rt") as f_map:
+            # self.logger.debug("%s: reading map %s" % (sancov, map))
+            bits = int(f_map.readline())
+            if bits != 32 and bits != 64:
+                raise Exception('Wrong bits size in the map')
+            for line in f_map:
+                parts = line.rstrip().split()
+                mem_map.append((int(parts[0], 16), int(parts[1], 16), int(parts[2], 16), ' '.join(parts[3:])))
+        f_map.close()
+        mem_map.sort(key=lambda m: m[0])
+        mem_map_keys = [m[0] for m in mem_map]
+
+        with open(raw, mode="rb") as f:
+            # self.logger.debug("%s: unpacking %s" % (sancov, raw))
+            f.seek(0, 2)
+            size = f.tell()
+            f.seek(0, 0)
+            pcs = struct.unpack_from(self._type_code_for_struct(bits) * (size * 8 / bits), f.read(size))
+            mem_map_pcs = [[] for i in range(0, len(mem_map))]
+            for pc in pcs:
+                if pc == 0:
+                    continue
+                map_idx = bisect.bisect(mem_map_keys, pc) - 1
+                (start, end, base, module_path) = mem_map[map_idx]
+
+                assert pc >= start
+                if pc >= end:
+                    # self.logger.debug("warning: %s: pc %x outside of any known mapping" % (sancov, pc))
+                    continue
+                mem_map_pcs[map_idx].append(pc - base)
+
+            for ((start, end, base, module_path), pc_list) in zip(mem_map, mem_map_pcs):
+                if len(pc_list) == 0: continue
+                assert raw.endswith('.sancov.raw')
+                dst_path = os.path.join(self.sancov_path,
+                                        module_path + '.' + os.path.basename(raw)[:-4])
+                # self.logger.debug("%s: writing %d PCs to %s" % (sancov, len(pc_list), dst_path))
+                sorted_pc_list = sorted(pc_list)
+                pc_buffer = struct.pack(self._type_code_for_struct(bits) * len(pc_list), *sorted_pc_list)
+                with open(dst_path, 'ab+') as f2:
+                    f2.write(array.array('I', self._magic_for_bits(bits)).tostring())
+                    f2.seek(0, 2)
+                    f2.write(pc_buffer)
+                f2.close()
+        f.close()
+        return dst_path
+
+
+    def get_cov_file(self):
+        sancov_raw_name = None
+        sancov_name = None
+        sancov_file = None
+        if os.path.exists(self.sancov_path):
+            file_list = os.listdir(self.sancov_path)
+            for i in file_list:
+                if i.endswith(".sancov.raw"):
+                    sancov_raw_name = i
+                if i.endswith(".sancov"):
+                    sancov_name = i
+        if sancov_raw_name is None and sancov_name is None:
+            assert("[Error]Can't find sancov file!!!!")
+        if sancov_raw_name is not None:
+            sancov_raw_path = os.path.join(self.sancov_path, sancov_raw_name)
+            sancov_map_path = sancov_raw_path[: -3] + "map"
+            if not os.path.isfile(sancov_map_path):
+                assert ("[Error]Can't find sancov map file!!!!")
+            sancov_file = self.sancov_analyse(sancov_raw_path, sancov_map_path)
+        if sancov_name is not None:
+            sancov_file = os.path.join(self.sancov_path, sancov_name)
+        return sancov_file
+
 
     def get_bit_map(self, file):
         new_edge = 0
@@ -171,7 +248,8 @@ class GuidedTarget(BaseTarget):
             self.logger.error(traceback.format_exc())
             self.send_failure = True
         self.transmission_count += 1
-        trace_bits = self.get_bit_map(self.tfile)
+        sancov_file = self.get_cov_file()
+        trace_bits = self.get_bit_map(sancov_file)
         return response, trace_bits
 
     def post_test(self, test_num):
